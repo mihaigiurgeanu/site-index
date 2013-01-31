ec10a86b09544581f8552f796b0b4fd5c1d6ef51
Mihai Giurgeanu <mihai.giurgeanu@genesissoftware.ro>
2013-01-31 17:12:19 +0200

    Testare cu clj-web-crawl.

--- project.clj
+++ project.clj
@@ -3,5 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [com.jmeeks/clj-web-crawler "0.1.0-SNAPSHOT"]
+                 [org.clojars.kjw/commons-httpclient "3.1"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 ]
   :main site-index.core)
--- src/site_index/core.clj
+++ src/site_index/core.clj
@@ -1,7 +1,12 @@
 (ns site-index.core
+  (:use clj-web-crawler)
   (:gen-class))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (let [clj-ws (client "http://86.120.215.146:8080")
+        home  (method "/")] 
+    (crawl clj-ws home
+           (println (.getStatusCode home))  
+           (println (response-str home)))))

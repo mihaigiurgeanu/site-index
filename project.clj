a5a7d8662e0cdf8028ecb2029ca9cd027182cf94
Mihai Giurgeanu <mihai.giurgeanu@genesissoftware.ro>
2013-09-16 11:22:20 +0300

    Set the web page of the project the link to git-hub.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject site-index "0.1.0-SNAPSHOT"
   :description "Creates/updates the Lucene search index of the pages of a web-site."
-  :url "http://example.com/FIXME"
+  :url "https://github.com/mihaigiurgeanu/site-index"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
